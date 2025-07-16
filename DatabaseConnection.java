cf4a718778e4cb4273938365272862c57cc4244f
BAIRAGONI MAHESH <bairagoni143@gmail.com>
2025-07-16 05:50:48 +0530

    Update DatabaseConnection.java

--- DatabaseConnection.java
+++ DatabaseConnection.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/vetpharma";
+    private static final String URL = "";// use your own URL while connecting//scheme 
     private static final String USER = "root"; // your MySQL username
-    private static final String PASSWORD = "Mahesh@06122001"; // your password
+    private static final String PASSWORD = ""; // your password
 
     public static Connection getConnection() {
         Connection conn = null;
